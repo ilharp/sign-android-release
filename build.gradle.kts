1aff072d294460804e4da94229ef4f9fa7384056
Il Harper <hi@ilharper.com>
2022-06-18 11:33:53 +0800

    chore: init gradle project

---
+++ build.gradle.kts
@@ -0,0 +1,59 @@
+buildscript {
+    repositories {
+        jcenter()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.diffplug.spotless:spotless-plugin-gradle:6.6.1")
+    }
+}
+
+plugins {
+    kotlin("js") version "1.7.0"
+}
+
+group = "com.ilharper"
+version = "0.1.0"
+
+allprojects {
+    repositories {
+        jcenter()
+        mavenCentral()
+    }
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-nodejs:0.0.7")
+}
+
+kotlin {
+    js(LEGACY) {
+        binaries.executable()
+        nodejs {
+        }
+    }
+}
+
+apply(plugin = "com.diffplug.spotless")
+configure<com.diffplug.gradle.spotless.SpotlessExtension> {
+    format("misc") {
+        target("**/*.md", "**/*.editorconfig", "**/*.gitignore", "**/*.gitattributes")
+        trimTrailingWhitespace()
+        indentWithSpaces()
+        endWithNewline()
+    }
+
+    kotlin {
+        ktlint().setUseExperimental(true)
+        trimTrailingWhitespace()
+        indentWithSpaces()
+        endWithNewline()
+    }
+
+    kotlinGradle {
+        ktlint().setUseExperimental(true)
+        trimTrailingWhitespace()
+        indentWithSpaces()
+        endWithNewline()
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "sign-android-release"
