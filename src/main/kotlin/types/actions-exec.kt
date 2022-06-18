4aacdb13d8fce695c39452fcdb01d6857543505a
Il Harper <hi@ilharper.com>
2022-06-18 12:03:03 +0800

    chore: cleanup types

--- src/main/kotlin/types/actions-exec.kt
+++ src/main/kotlin/types/actions-exec.kt
@@ -5,19 +5,7 @@
 package types.actions.exec
 
 import Buffer
-import org.khronos.webgl.*
-import org.w3c.dom.*
-import org.w3c.dom.events.*
-import org.w3c.dom.parsing.*
-import org.w3c.dom.svg.*
-import org.w3c.dom.url.*
-import org.w3c.fetch.*
-import org.w3c.files.*
-import org.w3c.notifications.*
-import org.w3c.performance.*
-import org.w3c.workers.*
-import org.w3c.xhr.*
-import kotlin.js.*
+import kotlin.js.Promise
 
 
 @JsModule("@actions/exec")
